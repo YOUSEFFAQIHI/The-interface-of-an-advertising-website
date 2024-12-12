2c0d3271499feaf8ff160685afa99d89825549a0
Yousef Faqihi <143995538+YOUSEFFAQIHI@users.noreply.github.com>
2024-12-12 07:29:07 +0300

    The interface of an advertising website
    The interface has been created for viewing only if you notice when accessing the links there is no event

---
+++ progrm.js
@@ -0,0 +1,13 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+let txt =  document.querySelector('.txt');
+
+function typePhone(clorPhone){
+    img.src = clorPhone;
+}
+function colores(color){
+    container.style.background = color;
+}
+function texts(text){
+    txt.style.color = text;
+}
